7bdf58627ea3ce09be5d728a6d2c5f07e369f27a
robertshc <herman.c.roberts.iii@gmail.com>
2019-04-15 15:51:50 -0400

    armor scraped

--- index.ts
+++ index.ts
@@ -3,21 +3,22 @@ import Axios from 'axios';// http request library
 import { v1 as neo4j } from 'neo4j-driver';//for the data base
 import { createReadStream, writeFileSync, readFile } from 'fs'//used to play with files
 import * as readline from 'readline';
+import { print } from 'util';
 const driver = neo4j.driver('bolt://hyenacros.com:7687', neo4j.auth.basic('neo4j', 'cheesecake'));
 const session = driver.session();
 const url = 'http://legacy.aonprd.com/coreRulebook/equipment.html';//for pulling the sorcers spell to change use the correct page
 Axios.get(url).then(res => {
     const $: JQueryStatic = cheerio.load(res.data);//dont change
-    const weapons = [];
-    let handed: any = 'unarmed';
+    const ArmorOrShield = [];
+    let Type: any = 'unarmed';
     let pro: any = '';
-    $('#table-6-4-weapons').children().each(function (i) {//jquery selector #id . is for class
-        let weapon: any = {};
+    $('#table-6-6-armor-and-shields').children().each(function (i) {//jquery selector #id . is for class
         if ($(this).is('thead')) {
             $(this).children().each(function (k) {
                 $(this).children().each(function (j) {
                     if (j == 0) {
-                        pro = $(this).text();
+                        //pro = $(this).text();
+                        //console.log("this is pro" + pro);
                     }
 
                 })
@@ -26,61 +27,63 @@ Axios.get(url).then(res => {
         }
         if ($(this).is('tbody')) {
             $(this).children().each(function (k) {
+                let armor: any = {};
                 $(this).children().each(function (j) {
-                    weapon.proficiency = pro;
                     if (j == 0) {
                         if ($(this).attr('colspan') == '9') {
-                            handed = $(this).text();
+                            Type = $(this).text();
                         }
                         else {
-                            weapon.type1 = handed;
-                            weapon.name = $(this).text();
+                            armor.type1 = Type;
+                            armor.name = $(this).text();
+                            console.log($(this).text());
                         }
 
                     }
                     else if (j == 1) {
                         if ($(this).text() != '—') {
-                            weapon.cost = $(this).text();
+                            armor.cost = $(this).text();
                         }
                     }
                     else if (j == 2) {
                         if ($(this).text() != '—') {
-                            weapon.dmgS = $(this).text();
+                            armor.bonus = $(this).text();
                         }
                     }
                     else if (j == 3) {
                         if ($(this).text() != '—') {
-                            weapon.dmgM = $(this).text();
+                            armor.maxDexBonus = $(this).text();
                         }
                     }
                     else if (j == 4) {
                         if ($(this).text() != '—') {
-                            weapon.critMultiplier = $(this).text();
+                            armor.ACP = $(this).text();
                         }
                     }
                     else if (j == 5) {
                         if ($(this).text() != '—') {
-                            weapon.rangeIncrement = $(this).text();
+                            armor.ArcaneSpellFailure = $(this).text();
                         }
                     }
                     else if (j == 6) {
                         if ($(this).text() != '—') {
-                            weapon.weight = $(this).text();
+                            armor.speed30 = $(this).text();
                         }
                     }
                     else if (j == 7) {
                         if ($(this).text() != '—') {
-                            weapon.dmgType = $(this).text();
+                            armor.speed20 = $(this).text();
                         }
                     }
                     else if (j == 8) {
                         if ($(this).text() != '—') {
-                            weapon.special = $(this).text();
+                            armor.weight = $(this).text();
                         }
                     }
                 })
-                if (Object.keys(weapon).length > 0) {
-                    weapons.push(weapon);
+                //ArmorOrShield.push(armor);
+                if (Object.keys(armor).length > 0) {
+                    ArmorOrShield.push(armor);
                 }
             })
 
@@ -95,7 +98,7 @@ Axios.get(url).then(res => {
     levels.forEach((level, i) => {
         query += `set l${i + 1}.knownSpells = [${level}]\n`;
     });*/
-    writeFileSync("weapons.json", JSON.stringify({ weapons }, null, 4))//level is the data 
+    writeFileSync("Armor.json", JSON.stringify({ ArmorOrShield }, null, 4))//level is the data 
     //return session.run(query);// do not call till ready 
 }).catch(err => {
     console.log(err);
